1dc1017263edd76f903be6f67607e985692d564b
Jaden Nguyen <nguyenthucanh422@gmail.com>
2023-10-14 16:27:09 -0400

    challenge completed, need better styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,54 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    @status = completed_yet(@task.completed)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to task_path, status: :see_other
+  end
+
+  private
+
+  def completed_yet(task_status)
+    @completion_status = ''
+
+    if task_status == true
+      @completion_status = 'This task is completed.'
+    else
+      @completion_status = 'This task is not completed yet.'
+    end
+
+    @completion_status
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # Add
+  get 'new', to: 'tasks#new', as: 'new'
+  post 'tasks', to: 'tasks#create'
+  # Edit
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'taskedit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # Destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
